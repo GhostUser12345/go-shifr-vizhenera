7ae37efb6bf7a7d7e7b35621bf1a51d736ae64ad
Alexander Nikolaev <nikolaevsailing@mail.ru>
2022-04-07 20:21:42 +0300

    feat: broken encryptor

--- vizhener.go
+++ vizhener.go
@@ -5,5 +5,22 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, Vizhener!")
+	//var message = "Hello, Vizhener!"
+	var encryptedMessage = "CSOITEUIWUIZNSROCNKFD"
+	var keyWord = "GOLANG"
+	var decryptedMessage = decode(encryptedMessage, keyWord)
+	fmt.Println(decryptedMessage)
+}
+
+func decode(encryptedMessage string, keyWord string) string {
+	var result = ""
+	var keyWordLength = len(keyWord)
+	for i := 0; i < len(encryptedMessage); i++ {
+		var currentLetter = encryptedMessage[i] + byte(keyWord[i%keyWordLength])
+		if currentLetter > 'Z' {
+			currentLetter -= 26
+		}
+		result += string(currentLetter)
+	}
+	return result
 }
