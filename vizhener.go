5b383c8eff934a744eeda3ea6223cca5d3c753c4
Alexander Nikolaev <nikolaevsailing@mail.ru>
2022-04-09 15:51:06 +0300

    feat: encoder

--- vizhener.go
+++ vizhener.go
@@ -5,11 +5,15 @@ import (
 )
 
 func main() {
-	//var message = "Hello, Vizhener!"
-	var encryptedMessage = "CSOITEUIWUIZNSROCNKFD"
-	var keyWord = "GOLANG"
-	var decryptedMessage = decode(encryptedMessage, keyWord)
-	fmt.Println(decryptedMessage)
+	var encryptedMessageDecode = "CSOITEUIWUIZNSROCNKFD"
+	var keyWordDecode = "GOLANG"
+	var decryptedMessageDecode = decode(encryptedMessageDecode, keyWordDecode)
+	fmt.Println(decryptedMessageDecode)
+
+	var decryptedMessageEncode = "WEDIGYOULUVTHEGOPHERS"
+	var keyWordEncode = "GOLANG"
+	var encryptedMessageEncode = encode(decryptedMessageEncode, keyWordEncode)
+	fmt.Println(encryptedMessageEncode)
 }
 
 func decode(encryptedMessage string, keyWord string) string {
@@ -20,6 +24,20 @@ func decode(encryptedMessage string, keyWord string) string {
 		var currentLetter = encryptedMessage[i] - move
 		if currentLetter < 'A' {
 			currentLetter += 26
+		}
+		result += string(currentLetter)
+	}
+	return result
+}
+
+func encode(decryptedMessage string, keyWord string) string {
+	var result = ""
+	var keyWordLength = len(keyWord)
+	for i := 0; i < len(decryptedMessage); i++ {
+		var move = byte(keyWord[i%keyWordLength]) - 'A'
+		var currentLetter = decryptedMessage[i] + move
+		if currentLetter > 'Z' {
+			currentLetter -= 26
 		}
 		result += string(currentLetter)
 	}
