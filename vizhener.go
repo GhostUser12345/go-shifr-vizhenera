96e9268b0ab72e2f45ba71584e9167c6661f139b
Alexander Nikolaev <nikolaevsailing@mail.ru>
2022-04-09 09:47:32 +0300

    fix: shift variable

--- vizhener.go
+++ vizhener.go
@@ -16,9 +16,10 @@ func decode(encryptedMessage string, keyWord string) string {
 	var result = ""
 	var keyWordLength = len(keyWord)
 	for i := 0; i < len(encryptedMessage); i++ {
-		var currentLetter = encryptedMessage[i] + byte(keyWord[i%keyWordLength])
-		if currentLetter > 'Z' {
-			currentLetter -= 26
+		var move = byte(keyWord[i%keyWordLength]) - 'A'
+		var currentLetter = encryptedMessage[i] - move
+		if currentLetter < 'A' {
+			currentLetter += 26
 		}
 		result += string(currentLetter)
 	}
